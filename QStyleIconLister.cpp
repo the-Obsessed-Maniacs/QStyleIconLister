e44a452489a8a6917894b1711c8b41b159d9910e
St0fF-NPL-ToM <st0ff.npl@gmail.com>
2025-01-01 22:54:44 +0100

    Major enhancements
    - added a QStyle-Selector and a Displayer of current colorScheme type (if it's dark or light)
    - put the "old content" into a tab widget's page
    - added a second page to list all QIcon::ThemeIcon

--- QStyleIconLister.cpp
+++ QStyleIconLister.cpp
@@ -1,15 +1,16 @@
 #include "QStyleStandardIcon.h"
 
 #include <QApplication>
-#include <QDialog>
+#include <QComboBox>
 #include <QGridLayout>
 #include <QLabel>
 #include <QPainter>
 #include <QPushButton>
 #include <QStyle>
 #include <QStyleFactory>
+#include <QStyleHints>
+#include <QTabWidget>
 #include <QToolButton>
-#include <QWindow>
 
 const char *strings[] = {
 	"SP_TitleBarMenuButton",
@@ -93,108 +94,317 @@ const char *strings[] = {
 	"SP_TabCloseButton",
 };
 
+const char *TIs[] = {
+	"AddressBookNew",
+	"ApplicationExit",
+	"AppointmentNew",
+	"CallStart",
+	"CallStop",
+	"ContactNew",
+	"DocumentNew",
+	"DocumentOpen",
+	"DocumentOpenRecent",
+	"DocumentPageSetup",
+	"DocumentPrint",
+	"DocumentPrintPreview",
+	"DocumentProperties",
+	"DocumentRevert",
+	"DocumentSave",
+	"DocumentSaveAs",
+	"DocumentSend",
+	"EditClear",
+	"EditCopy",
+	"EditCut",
+	"EditDelete",
+	"EditFind",
+	"EditPaste",
+	"EditRedo",
+	"EditSelectAll",
+	"EditUndo",
+	"FolderNew",
+	"FormatIndentLess",
+	"FormatIndentMore",
+	"FormatJustifyCenter",
+	"FormatJustifyFill",
+	"FormatJustifyLeft",
+	"FormatJustifyRight",
+	"FormatTextDirectionLtr",
+	"FormatTextDirectionRtl",
+	"FormatTextBold",
+	"FormatTextItalic",
+	"FormatTextUnderline",
+	"FormatTextStrikethrough",
+	"GoDown",
+	"GoHome",
+	"GoNext",
+	"GoPrevious",
+	"GoUp",
+	"HelpAbout",
+	"HelpFaq",
+	"InsertImage",
+	"InsertLink",
+	"InsertText",
+	"ListAdd",
+	"ListRemove",
+	"MailForward",
+	"MailMarkImportant",
+	"MailMarkRead",
+	"MailMarkUnread",
+	"MailMessageNew",
+	"MailReplyAll",
+	"MailReplySender",
+	"MailSend",
+	"MediaEject",
+	"MediaPlaybackPause",
+	"MediaPlaybackStart",
+	"MediaPlaybackStop",
+	"MediaRecord",
+	"MediaSeekBackward",
+	"MediaSeekForward",
+	"MediaSkipBackward",
+	"MediaSkipForward",
+	"ObjectRotateLeft",
+	"ObjectRotateRight",
+	"ProcessStop",
+	"SystemLockScreen",
+	"SystemLogOut",
+	"SystemSearch",
+	"SystemReboot",
+	"SystemShutdown",
+	"ToolsCheckSpelling",
+	"ViewFullscreen",
+	"ViewRefresh",
+	"ViewRestore",
+	"WindowClose",
+	"WindowNew",
+	"ZoomFitBest",
+	"ZoomIn",
+	"ZoomOut",
+	"AudioCard",
+	"AudioInputMicrophone",
+	"Battery",
+	"CameraPhoto",
+	"CameraVideo",
+	"CameraWeb",
+	"Computer",
+	"DriveHarddisk",
+	"DriveOptical",
+	"InputGaming",
+	"InputKeyboard",
+	"InputMouse",
+	"InputTablet",
+	"MediaFlash",
+	"MediaOptical",
+	"MediaTape",
+	"MultimediaPlayer",
+	"NetworkWired",
+	"NetworkWireless",
+	"Phone",
+	"Printer",
+	"Scanner",
+	"VideoDisplay",
+	"AppointmentMissed",
+	"AppointmentSoon",
+	"AudioVolumeHigh",
+	"AudioVolumeLow",
+	"AudioVolumeMedium",
+	"AudioVolumeMuted",
+	"BatteryCaution",
+	"BatteryLow",
+	"DialogError",
+	"DialogInformation",
+	"DialogPassword",
+	"DialogQuestion",
+	"DialogWarning",
+	"FolderDragAccept",
+	"FolderOpen",
+	"FolderVisiting",
+	"ImageLoading",
+	"ImageMissing",
+	"MailAttachment",
+	"MailUnread",
+	"MailRead",
+	"MailReplied",
+	"MediaPlaylistRepeat",
+	"MediaPlaylistShuffle",
+	"NetworkOffline",
+	"PrinterPrinting",
+	"SecurityHigh",
+	"SecurityLow",
+	"SoftwareUpdateAvailable",
+	"SoftwareUpdateUrgent",
+	"SyncError",
+	"SyncSynchronizing",
+	"UserAvailable",
+	"UserOffline",
+	"WeatherClear",
+	"WeatherClearNight",
+	"WeatherFewClouds",
+	"WeatherFewCloudsNight",
+	"WeatherFog",
+	"WeatherShowers",
+	"WeatherSnow",
+	"WeatherStorm",
+};
+
 int main( int argc, char *argv[] )
 {
 	QApplication a( argc, argv );
-	a.setStyle( QStyleFactory::create(
-		[ &a ]()
-		{
-			auto styles = QStyleFactory::keys();
-			auto res	= styles.last();
-			if ( styles.contains( "Fusion" ) ) res = "Fusion";
-			qDebug() << "styles: ->selected:" << res << " ->available:" << styles;
-			return res;
-		}() ) );
-	QWidget w;
-	w.setWindowTitle( PROJECT_NAME );
-	auto gl	  = new QGridLayout();
-	auto cntx = 5;
-	auto cnty = qCeil( float( QStyle::NStandardPixmap ) / cntx );
-	for ( auto y( 0 ); y < cnty; ++y )
-		for ( auto x( 0 ), i( y * cntx ); x < cntx; ++i, ++x )
-			if ( i < QStyle::NStandardPixmap )
-			{
-				auto tb = new QToolButton( &w );
-				tb->setIconSize( { 32, 32 } );
-				tb->setIcon( StandardIcon::get( static_cast< QStyle::StandardPixmap >( i ) ) );
-				tb->setText( strings[ i ] );
-				tb->setToolButtonStyle( Qt::ToolButtonTextBesideIcon );
-				w.connect(
-					tb, &QToolButton::clicked,
-					[ &w, i, &a, tb ]( bool checked )
-					{
-						auto icon = StandardIcon::get( static_cast< QStyle::StandardPixmap >( i ) );
-						auto sizes = icon.availableSizes();
-						qDebug() << "icon sizes:" << sizes;
-						auto l = new QLabel( nullptr, Qt::WindowType::Tool );
-						l->setAlignment( Qt::AlignCenter );
-						if ( sizes.isEmpty() )
+	QComboBox	*cb = new QComboBox;
+	cb->addItems( QStyleFactory::keys() );
+	cb->connect( cb, &QComboBox::currentTextChanged,
+				 [ &a ]( const QString &text ) { a.setStyle( QStyleFactory::create( text ) ); } );
+	QHBoxLayout *hla = new QHBoxLayout;
+	hla->addWidget( new QLabel( "Style:" ) );
+	hla->addWidget( cb );
+	hla->addStretch();
+	constexpr const char *ns[] = { "unknown", "bright scheme", "dark scheme" };
+	int					  s	   = static_cast< int >( a.styleHints()->colorScheme() );
+	hla->addWidget( new QLabel( ns[ s ] ) );
+	QVBoxLayout *vla = new QVBoxLayout;
+	vla->addLayout( hla );
+
+	auto createSPs = [ & ]()
+	{
+		QWidget *w	  = new QWidget;
+		auto	 gl	  = new QGridLayout();
+		auto	 cntx = 5;
+		auto	 cnty = qCeil( float( QStyle::NStandardPixmap ) / cntx );
+		for ( auto y( 0 ); y < cnty; ++y )
+			for ( auto x( 0 ), i( y * cntx ); x < cntx; ++i, ++x )
+				if ( i < QStyle::NStandardPixmap )
+				{
+					auto tb = new QToolButton( w );
+					tb->setIconSize( { 32, 32 } );
+					tb->setIcon( StandardIcon::get( static_cast< QStyle::StandardPixmap >( i ) ) );
+					tb->setText( strings[ i ] );
+					tb->setToolButtonStyle( Qt::ToolButtonTextBesideIcon );
+					w->connect(
+						tb, &QToolButton::clicked,
+						[ w, i, tb ]( bool checked )
 						{
-							auto  baseIco = a.style()->standardIcon( QStyle::SP_DialogYesButton );
-							QIcon myIcon;
-							sizes	 = baseIco.availableSizes();
-							auto psz = sizes.last();
-							psz		 = { 2 * psz.width(), 4 * psz.height() };
-							QPixmap	 pm{ psz };
-							QPainter p( &pm );
-							for ( auto sz : sizes )
+							auto icon =
+								StandardIcon::get( static_cast< QStyle::StandardPixmap >( i ) );
+							auto sizes = icon.availableSizes();
+							qDebug() << "icon sizes:" << sizes;
+							auto l = new QLabel( nullptr, Qt::WindowType::Tool );
+							l->setAlignment( Qt::AlignCenter );
+							if ( sizes.isEmpty() )
 							{
-								qDebug() << "build size:" << sz
-										 << "actual size:" << baseIco.actualSize( sz );
-								auto posy = psz.height() / 8 - ( sz.height() >> 1 );
-								for ( auto [ mode, group ] :
-									  QList< QPair< QIcon::Mode, QPalette::ColorGroup > >{
-										  { QIcon::Normal, QPalette::ColorGroup::Normal },
-										  { QIcon::Disabled, QPalette::ColorGroup::Disabled },
-										  { QIcon::Active, QPalette::ColorGroup::Active },
-										  { QIcon::Selected, QPalette::ColorGroup::Current } } )
+								auto baseIco =
+									qApp->style()->standardIcon( QStyle::SP_DialogYesButton );
+								QIcon myIcon;
+								sizes	 = baseIco.availableSizes();
+								auto psz = sizes.last();
+								psz		 = { 2 * psz.width(), 4 * psz.height() };
+								QPixmap	 pm{ psz };
+								QPainter p( &pm );
+								for ( auto sz : sizes )
 								{
-									p.drawPixmap( QRect{ QPoint{ 0, posy }, sz },
-												  baseIco.pixmap( sz, mode, QIcon::On ) );
-									qreal szf = 4;
-									QPen  p1( p.pen() );
-									p1.setColor( a.palette().color(
-										group, QPalette::ColorRole::ButtonText ) );
-									p1.setCapStyle( Qt::RoundCap );
-									QPen p2( p1 );
-									p1.setWidthF( sz.width() / ( 10. ) );
-									p2.setWidthF( sz.width() / ( 20. ) );
-									p2.setColor(
-										a.palette().color( group, QPalette::ColorRole::Shadow ) );
-									auto lns = {
-										QLineF( { sz.width() / szf, posy + sz.height() / 2. },
-												{ ( szf - 1 ) * sz.width() / szf,
-												  posy + sz.height() / 2. } ),
-										QLineF( { sz.width() / 2., posy + sz.height() / szf },
-												{ sz.width() / 2.,
-												  posy + ( szf - 1 ) * sz.height() / szf } ) };
-									for ( auto pe : { p1, p2 } ) p.setPen( pe ), p.drawLines( lns );
-									posy += psz.height() / 4;
+									qDebug() << "build size:" << sz
+											 << "actual size:" << baseIco.actualSize( sz );
+									auto posy = psz.height() / 8 - ( sz.height() >> 1 );
+									for ( auto [ mode, group ] :
+										  QList< QPair< QIcon::Mode, QPalette::ColorGroup > >{
+											  { QIcon::Normal, QPalette::ColorGroup::Normal },
+											  { QIcon::Disabled, QPalette::ColorGroup::Disabled },
+											  { QIcon::Active, QPalette::ColorGroup::Active },
+											  { QIcon::Selected, QPalette::ColorGroup::Current } } )
+									{
+										p.drawPixmap( QRect{ QPoint{ 0, posy }, sz },
+													  baseIco.pixmap( sz, mode, QIcon::On ) );
+										qreal szf = 4;
+										QPen  p1( p.pen() );
+										p1.setColor( qApp->palette().color(
+											group, QPalette::ColorRole::ButtonText ) );
+										p1.setCapStyle( Qt::RoundCap );
+										QPen p2( p1 );
+										p1.setWidthF( sz.width() / ( 10. ) );
+										p2.setWidthF( sz.width() / ( 20. ) );
+										p2.setColor( qApp->palette().color(
+											group, QPalette::ColorRole::Shadow ) );
+										auto lns = {
+											QLineF( { sz.width() / szf, posy + sz.height() / 2. },
+													{ ( szf - 1 ) * sz.width() / szf,
+													  posy + sz.height() / 2. } ),
+											QLineF( { sz.width() / 2., posy + sz.height() / szf },
+													{ sz.width() / 2.,
+													  posy + ( szf - 1 ) * sz.height() / szf } ) };
+										for ( auto pe : { p1, p2 } )
+											p.setPen( pe ), p.drawLines( lns );
+										posy += psz.height() / 4;
+									}
+									p.translate( sz.width(), 0 );
 								}
-								p.translate( sz.width(), 0 );
-							}
-							p.end();
-							l->setPixmap( pm );
-						} else l->setPixmap( icon.pixmap( sizes.last(), 2. ) );
-						l->setWindowTitle( tb->text() );
-						l->show();
-					} );
-				gl->addWidget( tb, y, x );
-			} else {
-				auto pb = new QPushButton( "only Icons", &w );
-				pb->setCheckable( true );
-				w.connect( pb, &QPushButton::clicked,
-						   [ & ]( bool checked )
-						   {
-							   pb->setText( checked ? "Icons with text" : "only Icons" );
-							   for ( auto tb : w.findChildren< QToolButton * >() )
-								   tb->setToolButtonStyle( checked ? Qt::ToolButtonIconOnly
-																   : Qt::ToolButtonTextBesideIcon );
-						   } );
-				gl->addWidget( pb, y, x );
-				y = cnty, x = cntx;
-			}
-	w.setLayout( gl );
+								p.end();
+								l->setPixmap( pm );
+							} else l->setPixmap( icon.pixmap( sizes.last(), 2. ) );
+							l->setWindowTitle( tb->text() );
+							l->show();
+						} );
+					gl->addWidget( tb, y, x );
+				} else {
+					auto pb = new QPushButton( "only Icons", w );
+					pb->setCheckable( true );
+					w->connect( pb, &QPushButton::clicked,
+								[ w, pb ]( bool checked )
+								{
+									pb->setText( checked ? "Icons with text" : "only Icons" );
+									for ( auto tb : w->findChildren< QToolButton * >() )
+										tb->setToolButtonStyle(
+											checked ? Qt::ToolButtonIconOnly
+													: Qt::ToolButtonTextBesideIcon );
+								} );
+					gl->addWidget( pb, y, x );
+					y = cnty, x = cntx;
+				}
+		w->setLayout( gl );
+		return w;
+	};
+
+	auto createTIs = [ & ]()
+	{
+		auto w	  = new QWidget;
+		auto n	  = static_cast< int >( QIcon::ThemeIcon::NThemeIcons );
+		auto gl	  = new QGridLayout();
+		auto cntx = 5;
+		auto cnty = qCeil( float( n ) / cntx );
+		for ( auto y( 0 ); y < cnty; ++y )
+			for ( auto x( 0 ), i( y * cntx ); x < cntx; ++i, ++x )
+				if ( i < n )
+				{
+					auto tb = new QToolButton( w );
+					tb->setIconSize( { 32, 32 } );
+					tb->setIcon( QIcon::fromTheme( static_cast< QIcon::ThemeIcon >( i ) ) );
+					tb->setText( TIs[ i ] );
+					tb->setToolButtonStyle( Qt::ToolButtonTextBesideIcon );
+					gl->addWidget( tb, y, x );
+				} else {
+					auto pb = new QPushButton( "only Icons", w );
+					pb->setCheckable( true );
+					w->connect( pb, &QPushButton::clicked,
+								[ w, pb ]( bool checked )
+								{
+									pb->setText( checked ? "Icons with text" : "only Icons" );
+									for ( auto tb : w->findChildren< QToolButton * >() )
+										tb->setToolButtonStyle(
+											checked ? Qt::ToolButtonIconOnly
+													: Qt::ToolButtonTextBesideIcon );
+								} );
+					gl->addWidget( pb, y, x );
+					y = cnty, x = cntx;
+				}
+		w->setLayout( gl );
+		return w;
+	};
+
+	QTabWidget *tw = new QTabWidget;
+	tw->setWindowTitle( PROJECT_NAME );
+	tw->addTab( createSPs(), "QStyle Standard Pixmaps" );
+	tw->addTab( createTIs(), "QIcon::fromTheme" );
+	vla->addWidget( tw );
+	QWidget w;
+	w.setLayout( vla );
 	w.show();
 	return a.exec();
 }
